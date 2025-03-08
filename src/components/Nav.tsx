d8f396b1711ad409b844d626fcb4669e23af6f87
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2025-03-08 22:52:46 +0530

    feat: create header

---
+++ src/components/Nav.tsx
@@ -0,0 +1,79 @@
+import styled from "styled-components"
+import { NavLink as RouterNavLink } from "react-router-dom"
+import { ShoppingCart } from "lucide-react"
+
+const Nav = styled.nav`
+  width: 100%;
+  display: grid;
+  place-content: center;
+  gap: 2rem;
+  padding: 2rem;
+
+  h1 {
+    text-align: center;
+    font-size: 1.6rem;
+  }
+
+  ul {
+    display: flex;
+    justify-content: center;
+    gap: 1rem;
+    list-style-type: none;
+    padding: 0;
+  }
+`
+
+const NavLink = styled(RouterNavLink)`
+  &.active {
+    text-decoration: underline;
+  }
+`
+
+const Cart = styled.div`
+  position: relative;
+
+  span {
+    position: absolute;
+    top: -4px;
+    right: -8px;
+    display: grid;
+    place-content: center;
+    height: 16px;
+    width: 16px;
+    padding: 0.2rem;
+    background-color: black;
+    color: white;
+    border-radius: 50%;
+  }
+`
+
+const Header = () => {
+  return (
+    <header>
+      <Nav>
+        <h1>FAKESTORE</h1>
+        <ul>
+          <li>
+            <NavLink to="/" viewTransition>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/shop" viewTransition>
+              Shop
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/cart" viewTransition>
+              <Cart>
+                <ShoppingCart />
+                <span>{0}</span>
+              </Cart>
+            </NavLink>
+          </li>
+        </ul>
+      </Nav>
+    </header>
+  )
+}
+export default Header
---
+++ src/components/Root.tsx
@@ -0,0 +1,71 @@
+import { Outlet } from "react-router-dom"
+import { createGlobalStyle } from "styled-components"
+import Header from "./Nav"
+
+const Global = createGlobalStyle`
+:root {
+  font-size: 16px;
+  font-family: Raleway;
+}
+
+*, *::before, *::after {
+  box-sizing: border-box;
+}
+
+* {
+  margin: 0;
+}
+
+body {
+  line-height: 1.5;
+  -webkit-font-smoothing: antialiased;
+
+}
+
+img, picture, video, canvas, svg {
+  display: block;
+  max-width: 100%;
+}
+
+input, button, textarea, select {
+  font: inherit;
+}
+
+p, h1, h2, h3, h4, h5, h6 {
+  overflow-wrap: break-word;
+}
+
+p {
+  text-wrap: pretty;
+}
+h1, h2, h3, h4, h5, h6 {
+  text-wrap: balance;
+}
+
+#root, #__next {
+  isolation: isolate;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+a {
+  color: #000;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+}
+`
+
+const Root = () => {
+  return (
+    <>
+      <Global />
+      <Header />
+      <Outlet />
+    </>
+  )
+}
+export default Root
