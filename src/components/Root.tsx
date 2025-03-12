94e4fafef1cb8d341348d8729992cf3671ea4327
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2025-03-12 16:39:32 +0000

    feat: supply props to outlet context

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,82 +1,65 @@
+import { useEffect, useState } from "react"
+import axios, { CanceledError } from "axios"
 import { Outlet } from "react-router-dom"
-import { createGlobalStyle } from "styled-components"
+import { Global, Spinner } from "./Global"
 import Header from "./Nav"
-
-const Global = createGlobalStyle`
-:root {
-  font-size: 16px;
-  font-family: Raleway;
-}
-
-*, *::before, *::after {
-  box-sizing: border-box;
-}
-
-* {
-  margin: 0;
-}
-
-body {
-  line-height: 1.5;
-  -webkit-font-smoothing: antialiased;
-
-}
-
-img, picture, video, canvas, svg {
-  display: block;
-  max-width: 100%;
-}
-
-input, button, textarea, select {
-  font: inherit;
-}
-
-p, h1, h2, h3, h4, h5, h6 {
-  overflow-wrap: break-word;
-}
-
-p {
-  text-wrap: pretty;
-}
-h1, h2, h3, h4, h5, h6 {
-  text-wrap: balance;
-}
-
-#root, #__next {
-  isolation: isolate;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-}
-
-a {
-  color: #000;
-  text-decoration: none;
-  position: relative;
-
-  &.animated::after {
-    content: '';
-    position: absolute;
-    bottom: 2px;
-    left: 0;
-    height:1px;
-    width: 1px;
-    background-color: black;
-    transition: width .2s ease-out;
-  }
-
-  &.animated:hover::after {
-    width: 100%;
-  }
-}
-`
+import Product from "../product"
 
 const Root = () => {
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const [cart, setCart] = useState<Product[]>([])
+
+  useEffect(() => {
+    const abortController = new AbortController()
+
+    axios
+      .get("https://fakestoreapi.com/products", {
+        signal: abortController.signal,
+      })
+
+      .then(({ data }) => {
+        const products = data
+          .filter((product: Product) => product.category !== "electronics")
+          .map(({ id, title, price, image }: Product) => ({
+            id,
+            title,
+            price,
+            image,
+            quantity: 0,
+          }))
+
+        setProducts(products)
+      })
+
+      .catch((err) => {
+        setError(err instanceof CanceledError ? null : err.message)
+      })
+
+      .finally(() => setLoading(false))
+
+    return () => abortController.abort()
+  }, [])
+
+  if (loading) return <Spinner />
+  if (error) return <div>Error: {error}</div>
+
   return (
     <>
       <Global />
-      <Header />
-      <Outlet />
+      <Header cart={cart} />
+      <Outlet
+        context={{
+          products,
+          setProducts,
+          loading,
+          error,
+          cart,
+          setCart,
+          Spinner,
+        }}
+      />
     </>
   )
 }
